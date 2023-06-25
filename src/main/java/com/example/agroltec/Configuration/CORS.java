e079758b90201140c9b59f5aa79a5002bf0ad8e7
Mr.Agustin <47537652+AgenteAgherse@users.noreply.github.com>
2023-06-25 09:13:20 -0500

    Explicación CORS
    Se añade un comentario para ver como configurar el CORS para transacción de datos.

--- src/main/java/com/example/agroltec/Configuration/CORS.java
+++ src/main/java/com/example/agroltec/Configuration/CORS.java
@@ -9,7 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CORS implements WebMvcConfigurer {
-    
+
+    /**
+    * Para implementar una configuración válida entre el front y el back, hay que incluir la 
+    * configuración del CORS. En este ejemplo, hay que permitir la comunicación entre el programa
+    * y el sitio web o la app donde está el front (En este caso, el front de la aplicación se encuentra
+    * en el puerto 5173).
+    * Este es un ejemplo básico para brindar permisos de transacciones entre una aplicación realizada en React.js
+    * y Spring.
+    */
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
